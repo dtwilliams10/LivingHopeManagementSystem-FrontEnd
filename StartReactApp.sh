49de43aea38b9b4effbcb085d8135e7a2756ef19
dtwilliams10 <david.williams110913@gmail.com>
2018-07-29 21:05:22 -0400

    Updated script to start the Docker container if it isn't started already

--- StartReactApp.sh
+++ StartReactApp.sh
@@ -1,1 +1,3 @@
-docker run -d -v ${PWD}:/usr/src/app -v /usr/src/app/node_modules -p 3000:3000 --name=lhms-app lhms-app
+docker run -d -v ${PWD}:/usr/src/app -v /usr/src/app/node_modules -p 3000:3000 --name=lhms-app lhms-app;
+
+docker start lhms-app;
