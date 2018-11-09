64e2926579a940ae1f0f98619b396901d739da02
dtwilliams10 <david.williams110913@gmail.com>
2018-11-08 20:37:09 -0500

    Updated to remove more code for test calls

--- src/App.js
+++ src/App.js
@@ -20,8 +20,6 @@ class App extends Component {
         </header>
         <RTF />
         <MyUsers />
-        <p className="systemstatus">{this.state.apiResponse}</p>
-        <p className="systemStatus">{this.state.dbResponse}</p>
       </div>
     );
   }
