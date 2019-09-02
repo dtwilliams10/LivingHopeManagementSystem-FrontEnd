d6440382251d38dd11eed9f8d485f28e6f3c7818
dtwilliams10 <david.williams110913@gmail.com>
2019-09-02 14:20:44 -0400

    feat(SystemReport.tsx): Added input fields and Onclick to post data
    Added input fields for each part of the System Report and an OnClick event to post data to the API

--- src/pages/SystemReport/SystemReport.tsx
+++ src/pages/SystemReport/SystemReport.tsx
@@ -2,31 +2,77 @@ import React, { Component } from 'react';
 import HeaderBar from 'components/headerBar';
 import AppBar from 'components/AppBar';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+import axios from 'axios';
 
 const endpoint: string = 'values';
 const url: string = process.env.REACT_APP_URL + endpoint;
 console.log(url);
+
+interface SystemReport {
+  ReportID: number;
+  ReportName: string;
+  ReportDate: Date;
+  CreatedDate: Date;
+  UpdatedDate: Date;
+  SystemName: string;
+  SystemUpdate: string;
+  PersonnelUpdates: string;
+  CreativeIdeasAndEvaluations: string;
+  BarriersOrChallenges: string;
+  HowCanIHelpYou: string;
+  PersonalGrowthAndDevelopment: string;
+}
 class SystemReport extends Component {
-  componentWillMount() {
-    fetch(url)
-      .then(response => response.json())
-      .then(response => {
+  state = {
+    SystemReport
+  };
+  componentDidMount() {
+    axios.get(url).then(res => {
+      let response = res.data;
+      this.setState({ response });
+    });
+  }
+
+  handleSubmitClick() {
+    axios
+      .post(process.env.REACT_APP_URL + 'SystemReport')
+      .then(function(response) {
         console.log(response);
-        console.log(response.length);
-        this.setState({ systemReport: response });
       });
   }
 
   render() {
-    const SystemReport = JSON.stringify(this.state);
     return (
       <div className="App">
         <HeaderBar />
         <AppBar />
-        <p>This is text for the System Report Page</p>
-        <TextField required multiline variant="outlined" />
-        <p />
-        {SystemReport}
+        <div>
+          <form>
+            <TextField
+              multiline={true}
+              autoCorrect="true"
+              id="System Report Name"
+            ></TextField>
+            <TextField multiline={true} autoCorrect="true"></TextField>
+            <TextField multiline={true} autoCorrect="true"></TextField>
+            <TextField multiline={true} autoCorrect="true"></TextField>
+            <TextField multiline={true} autoCorrect="true"></TextField>
+            <TextField multiline={true} autoCorrect="true"></TextField>
+            <TextField multiline={true} autoCorrect="true"></TextField>
+            <TextField multiline={true} autoCorrect="true"></TextField>
+            <TextField multiline={true} autoCorrect="true"></TextField>
+            <TextField multiline={true} autoCorrect="true"></TextField>
+            <Button
+              variant="contained"
+              color="inherit"
+              onClick={this.handleSubmitClick}
+            >
+              Submit
+            </Button>
+          </form>
+        </div>
       </div>
     );
   }
