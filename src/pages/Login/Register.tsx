f929417076c7450b17259d70d8eb914061c873b1
dtwilliams10 <david.williams110913@gmail.com>
2020-08-29 12:51:22 -0400

    Rewrote registration form to use bootstrap-react

--- src/pages/Login/Register.tsx
+++ src/pages/Login/Register.tsx
@@ -1,177 +1,133 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HeaderBar from 'components/headerBar';
-import { Formik } from 'formik';
-import axios from 'axios';
 import { accountService } from '../../services/account.service';
-import { Redirect } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import { alertService } from 'services/alert.service';
+import { Form, Button, Col } from 'react-bootstrap';
 
-const endpoint: string = 'accounts/register';
-const url: string = process.env.REACT_APP_AAS + endpoint;
+export default function Registration() {
 
-class RegistrationPage extends React.Component {
-  render() {
-    if (accountService.userValue) {
-      return <Redirect to="/Home" />;
-    }
+  const history = useHistory();
 
-    function redirectPage(page) {
-      window.location.href = page;
-    }
+  const [values, setValues] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    acceptTerms: false,
+  })
 
-    return (
-      <div>
-        <HeaderBar />
-        <br />
-        <div className="registrationForm">
-        <Formik
-          initialValues={{
-            title: '',
-            firstName: '',
-            lastName: '',
-            email: '',
-            password: '',
-            confirmPassword: '',
-            acceptTerms: ''
-          }}
-          onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-              console.log('Registering user: ', values);
-              axios
-                .post(url, {
-                  firstName: values.firstName,
-                  lastName: values.lastName,
-                  email: values.email,
-                  password: values.password,
-                  confirmPassword: values.confirmPassword,
-                  acceptTerms: values.acceptTerms
-                })
-                .then(function() {
-                  redirectPage('/Login');
-                })
-                .catch(function(error) {
-                  console.log(error);
-                  //TODO: Need to add an onscreen notification for errors
-                });
-              setSubmitting(true);
-            }, 500);
-          }}
-
-          /* TODO: Need to add validation to all the fields */
-        >
-          {props => {
-            const {
-              values,
-              touched,
-              errors,
-              isSubmitting,
-              handleChange,
-              handleBlur,
-              handleSubmit
-            } = props;
-            return (
-              <form onSubmit={handleSubmit}>
-                <label htmlFor="firstName">First Name</label>
-                <input
-                  name="firstName"
-                  type="text"
-                  autoComplete="firstName"
-                  placeholder="Please enter your first name"
-                  value={values.firstName}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  className={errors.firstName && touched.firstName && 'error'}
-                />
-                {errors.firstName && touched.firstName && (
-                  <div className="input-feedback">{errors.firstName}</div>
-                )}
-
-                <label htmlFor="lastName">Last Name</label>
-                <input
-                  name="lastName"
-                  type="text"
-                  autoComplete="lastName"
-                  placeholder="Please enter your last name"
-                  value={values.lastName}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  className={errors.lastName && touched.lastName && 'error'}
-                />
-
-                {errors.email && touched.email && (
-                  <div className="input-feedback">{errors.email}</div>
-                )}
-                <label htmlFor="email">E-Mail Address</label>
-                <input
-                  name="email"
-                  type="text"
-                  autoComplete="email"
-                  placeholder="Please enter your email"
-                  value={values.email}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  className={
-                    errors.email && touched.email && 'error'
-                  }
-                />
-                {errors.email && touched.email && (
-                  <div className="input-feedback">{errors.email}</div>
-                )}
-                <label htmlFor="password">Password</label>
-                <input
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  placeholder="Please enter your password"
-                  value={values.password}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  className={errors.password && touched.password && 'error'}
-                />
-                {errors.password && touched.password && (
-                  <div className="input-feedback">{errors.password}</div>
-                )}
-                <label htmlFor="confirmPassword">Confirm Password</label>
-                <input
-                  name="confirmPassword"
-                  type="password"
-                  autoComplete="current-password"
-                  placeholder="Please confirm your password"
-                  value={values.confirmPassword}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  className={errors.confirmPassword && touched.confirmPassword && 'error'}
-                />
-                {errors.confirmPassword && touched.confirmPassword && (
-                  <div className="input-feedback">{errors.confirmPassword}</div>
-                )}
-                <label htmlFor="acceptTerms">I agree to the terms and conditions</label>
-                <input
-                  name="acceptTerms"
-                  type="checkbox"
-                  value={values.acceptTerms}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  className={errors.acceptTerms && touched.acceptTerms && 'error'}
-                  />
-                  {errors.acceptTerms && touched.acceptTerms && (
-                  <div className="input-feedback">{errors.acceptTerms}</div>
-                )}
-                <button type="submit" disabled={isSubmitting}>
-                  Submit
-                </button>
-                {/* TODO: Need to redirect to the login page after successful submission */}
-                <p className="registration-link">
-                  If you already have an account please click{' '}
-                  <a href="/Login">here</a> to login.
-                </p>
-              </form>
-            );
-          }}
-        </Formik>
-        </div>
-      </div>
-    );
+  const handleFirstNameChange = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      firstName: event.target.value,
+    }));
   }
-}
 
-export default RegistrationPage;
+  const handleLastNameChange = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      lastName: event.target.value,
+    }));
+  }
+
+  const handleEmailAddressChange = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      email: event.target.value,
+    }));
+  }
+
+  const handlePasswordChange = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      password: event.target.value,
+    }));
+  }
+
+  const handleConfirmPasswordChange = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      confirmPassword: event.target.value,
+    }));
+  }
+
+  const handleTandCChange = (event) => {
+    event.persist();
+    console.log(event.target.value);
+    if(event.target.value === "on") {
+      setValues((values) => ({
+        ...values,
+        acceptTerms: true
+      }));
+    } else {
+      setValues((values) => ({
+        ...values,
+        acceptTerms: false
+      }));
+    }
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    accountService.register(values)
+    .then(() => {
+      history.push('/Login');
+    })
+    .catch(error => {
+      console.log(error);
+      alertService.error(error, {keepAfterRouteChange: false});
+
+    })
+  }
+
+  return (
+    <div>
+      <HeaderBar/>
+      <br/>
+      <div className="registrationForm">
+        <Form>
+          <Form.Group controlId="firstName">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control type="text" placeholder="Please enter your first name." onChange={handleFirstNameChange}/>
+          </Form.Group>
+          <Form.Group controlId="lastName">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control type="text" placeholder="Please enter your last name." onChange={handleLastNameChange}/>
+          </Form.Group>
+          <Form.Group controlId="emailAddress">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control type="email" placeholder="Please enter your email address." onChange={handleEmailAddressChange}/>
+          </Form.Group>
+          <Form.Group controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Please enter a password" onChange={handlePasswordChange}/>
+          </Form.Group>
+          <Form.Group controlId="confirmPassword">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control type="password" placeholder="Please confirm your password" onChange={handleConfirmPasswordChange}/>
+          </Form.Group>
+          <Form.Row>
+            <Col xs="auto">
+              <Form.Label>I accept the terms and conditions</Form.Label>
+            </Col>
+            <Col>
+              <Form.Check type="checkbox" onChange={handleTandCChange}/>
+            </Col>
+          </Form.Row>
+          <Button onClick={handleSubmit}>
+            Register
+          </Button>
+        </Form>
+        <p>If you already have an account please click <a href="/Login">here</a>.</p>
+        <br/>
+      </div>
+    </div>
+  )
+}
