9b162c7890751c17a2fefa28ee15ea6dee0c8f58
dtwilliams10 <david.williams110913@gmail.com>
2020-08-29 15:07:45 -0400

    Rewrote login form to use bootstrap-react and added validation

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -3,19 +3,18 @@ import HeaderBar from "components/headerBar";
 import { useHistory } from 'react-router-dom'
 import { accountService } from "../../services/account.service";
 import { alertService } from "../../services/alert.service";
+import { Form, Button } from "react-bootstrap";
 
 export default function Login() {
 
     const history = useHistory();
 
-    const [submitted, setSubmitted] = useState(false);
-
-    const [error, setError] = useState(false);
-
     const [values, setValues] = useState({
         emailAddress: '',
         password: '',
     })
+
+    const [validated, setValidated] = useState(false);
 
     const handleEmailAddressInputChange = (event) => {
         event.persist();
@@ -34,58 +33,67 @@ export default function Login() {
     }
 
     const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+            setValidated(true);
+        } else {
         event.preventDefault();
-        setSubmitted(true);
         alertService.clear();
         accountService.login(values.emailAddress, values.password)
         .then(() => {
             history.push('/Home');
         })
         .catch(error => {
-            setError(true);
             alertService.error(error, { keepAfterRouteChange: false });
             history.push('/Login');
         });
     };
+}
 
     return (
-        <div>
-            <HeaderBar />
-            {error ? <span>An error has occured, please check your login information and try again. </span> : null}
-            <form className='login-form' onSubmit={handleSubmit}>
-                <br/>
-                {submitted && !values.emailAddress ? <span id="email-address-error">Please enter your email address</span> : null }
-                <label>Email Address</label>
-                <input
-                    id="email-address"
-                    className="form-field"
-                    type="text"
-                    placeholder="Please enter your email address"
-                    name="emailAddress"
-                    autoComplete="username"
-                    value={values.emailAddress}
-                    onChange={handleEmailAddressInputChange}
-                />
-                {submitted && !values.password ? <span id="password-error">Please enter a password</span> : null}
-                <label>Password</label>
-                <input
-                    id="password"
-                    className="form-field"
-                    type="password"
-                    placeholder="Please enter your password"
-                    name="password"
-                    autoComplete="current-password"
-                    value={values.password}
-                    onChange={handlePasswordInputChange}
-                />
-                <button
-                    id="login"
-                    type="submit"
-                    className="submit-button"
-                >Login</button>
-            </form>
-            <br/>
-            <div className='registration-link'>If you don't have an account, please register for one <a href="/Register">here</a>.</div>
+      <div>
+        <HeaderBar />
+        <div className="loginForm">
+          <br/>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Form.Group controlId="emailAddress">
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                placeholder="Please enter your email address."
+                onChange={handleEmailAddressInputChange}
+              />
+              <Form.Control.Feedback type="invalid">
+                Please enter your email address.
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                required
+                type="password"
+                placeholder="Please enter your password"
+                onChange={handlePasswordInputChange}
+              />
+              <Form.Control.Feedback type="invalid">
+                Please enter your password
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Button className="login-button" type="submit" >
+                Login
+            </Button>
+          </Form>
         </div>
+        <br/>
+        <div className="registration-link">
+          If you don't have an account, please register for one{" "}
+          <a href="/Register">here</a>.
+        </div>
+        <br/>
+        <div className="registration-link">If you have forgotten your password, please click <a href="/ForgotPassword">here</a>.</div>
+      </div>
     );
 };
