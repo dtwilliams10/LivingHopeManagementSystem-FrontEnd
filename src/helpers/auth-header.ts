14cee87a59c6d803ee0a83322bad36c99fc09cf4
dtwilliams10 <david.williams110913@gmail.com>
2021-06-20 18:52:04 -0400

    Added temp logging for authorization Bearer token

--- src/helpers/auth-header.ts
+++ src/helpers/auth-header.ts
@@ -4,6 +4,7 @@ export function authHeader() {
   // return authorization header with jwt token
   const currentUser = accountService.userValue;
   if (currentUser && currentUser.jwtToken) {
+    console.log("Authorization: `Bearer ", currentUser.jwtToken);
     return { Authorization: `Bearer ${currentUser.jwtToken}` };
   } else {
     console.log('User not retrieved');
