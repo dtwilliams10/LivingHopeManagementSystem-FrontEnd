e406a3545f8a0b3f1fd695debb25382b3625e5bc
dtwilliams10 <david.williams110913@gmail.com>
2021-06-20 18:49:54 -0400

    Fixed routing issue. Need to fix external routing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createBrowserHistory } from 'history';
 import 'App.css';
 
+export const history = createBrowserHistory();
+
 ReactDOM.render(
-  <Router>
+  <Router history={history}>
     <App />
   </Router>,
   document.getElementById('root')
