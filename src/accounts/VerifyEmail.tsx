bf13cd99ca9d81bc92858b180e421124d9486888
dtwilliams10 <david.williams110913@gmail.com>
2021-08-10 19:44:30 -0400

    Added a Verified status

--- src/accounts/VerifyEmail.tsx
+++ src/accounts/VerifyEmail.tsx
@@ -8,6 +8,7 @@ function VerifyEmail() {
     //eslint-disable-next-line
     const EmailStatus = {
         Verifying: 'Verifying',
+        Verified: 'Verified',
         Failed: 'Failed'
     }
 
@@ -18,22 +19,27 @@ function VerifyEmail() {
     useEffect(() => {
 
         const {token} = queryString.parse(window.location.search);
-
-        history.replace(window.location.pathname);
+        //This is being called every time the component loads. Need to stop calling once the email status changes. 
+        //history.replace(window.location.pathname);
         accountService.verifyEmail(token)
             .then(() => {
-                alertService.success('Verification successful, you may now login', { keepAfterRouteChange: true});
-                history.push('/Login');
+                alertService.success('Verification successful, you may now login', { keepAfterRouteChange: true}); 
+                setEmailStatus(EmailStatus.Verified); 
+                console.log(EmailStatus);
             })
             .catch(() => {
+                console.log()
                 setEmailStatus(EmailStatus.Failed);
             });
-    }, [EmailStatus, history]);
 
+    }, [EmailStatus, history]);
+    
     function getBody() {
         switch(emailStatus) {
             case EmailStatus.Verifying:
                 return <div>Verifying...</div>;
+            case EmailStatus.Verified:
+                return <div>Your email has been verified! Please go to <Link to="/Login">login</Link></div>
             case EmailStatus.Failed:
                 return <div>Verification failed. You can also verify your account using the <Link to="forgot-password">forgot password</Link> page.</div>
         }
