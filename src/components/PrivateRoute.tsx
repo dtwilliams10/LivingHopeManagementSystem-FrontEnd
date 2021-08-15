277145e57f392644e2c22c48b8388346b1944f81
dtwilliams10 <david.williams110913@gmail.com>
2021-08-14 20:55:47 -0400

    Scabbed in a workaround for the refresh/logout issue.

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -7,14 +7,16 @@ function PrivateRoute({ component: Component, roles, ...rest})
 {
     return (
         <Route {...rest} render={props => {
-            const user = accountService.userValue;
+            //This is failing to set and sending users back to the home page.
+            const user = localStorage.getItem('currentUser');
             if(!user) {
+                console.error("Redirecting to Login");
                 return <Redirect to={{pathname: '/Login', state: { from: props.location } }} />
             }
 
-            if (roles && roles.indexOf(user.role) === -1) {
+            /*if (roles && roles.indexOf(user.role) === -1) {
                 return <Redirect to={{ pathname: '/Home'}} />
-            }
+            }*/
 
             return <Component {...props} />
         }} />
