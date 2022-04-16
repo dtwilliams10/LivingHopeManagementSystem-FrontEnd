2b63018cededb3fbda05e024ac14e674840c6f27
Tyler Williams <david.williams110913@gmail.com>
2022-04-16 15:21:59 -0400

    Changed AppBar to ButtonAppBar

--- src/components/SystemReportList.tsx
+++ src/components/SystemReportList.tsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { Button } from '@material-ui/core';
 import { authHeader } from 'helpers/auth-header';
-import  ButtonAppBar from 'components/AppBar';
+import  ButtonAppBar from 'components/ButtonAppBar';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import AppBar from 'components/AppBar';
+import ButtonAppBar from 'components/ButtonAppBar';
 import 'App.css';
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <AppBar />
+        <ButtonAppBar />
         <div className="homePage">
           {/*TODO: Add a personalized welcome using the user name.*/}
           Hello, {sessionStorage.getItem("userFirstName")} {sessionStorage.getItem("userLastName")}. <br/>Welcome to the Living Hope Management System.
