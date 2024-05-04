cf699faae0c4fbe7e2d715c583b300bc464d5728
Tyler Williams <david.williams110913@gmail.com>
2024-05-04 09:56:17 -0400

    Updated vite config and test.Dockerfile

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,10 @@ export default defineConfig({
   plugins: [react(), viteTsconfigPaths()],
   server: {
     // this ensures that the browser opens upon server start
-    open: true,
+    open: false,
     // this sets a default port to 3000
     port: 3000,
+    strictPort: true,
+    host: true,
   },
 });
