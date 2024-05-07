9deb95094046a4abab276b0ab48ffa00cd2054b6
Tyler Williams <david.williams110913@gmail.com>
2024-05-06 21:55:27 -0400

    Added a temporary User Context

---
+++ src/app/context/UserContext.jsx
@@ -0,0 +1,63 @@
+import { createContext, useState } from "react";
+import agent from "../api/agent";
+
+const UserContext = createContext();
+
+export const UserContextProvider = () => {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState("");
+
+  const login = async (creds) => {
+    try {
+      const user = await agent.Accounts.login(creds);
+      setToken(user.jwtToken);
+      localStorage.setItem("jwt", user.jwtToken);
+      if (user) {
+        setUser(user);
+      }
+
+      console.log(user);
+      router.navigate("/home");
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
+
+  const logoff = async () => {
+    localStorage.clear();
+    setUser({});
+    router.navigate("/");
+  };
+
+  const register = async (creds) => {
+    try {
+      //TODO: creds.password - Need to salt and hash the password here before it's sumbitted.
+      const user = await agent.Accounts.register(creds);
+      setToken(user.jwtToken);
+      router.navigate("/home");
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
+
+  const contextValue = useMemo(
+    () => ({
+      user,
+      setUser,
+      token,
+      setToken,
+      login,
+      logoff,
+      register,
+    }),
+    []
+  );
+
+  return (
+    <UserContext.Provider value={contextValue}>{children}</UserContext.Provider>
+  );
+};
+
+export default UserContext;
