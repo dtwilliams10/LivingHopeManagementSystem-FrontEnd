d46a9b45f7662f8f9ebb6f0d27d68f739653f088
Tyler Williams <david.williams110913@gmail.com>
2024-05-06 21:52:29 -0400

    Removed systemReport store.

--- src/app/stores/store.ts
+++ src/app/stores/store.ts
@@ -1,21 +1,18 @@
 import { createContext, useContext } from "react";
 import CommonStore from "./commonStore";
 import ModalStore from "./modalStore";
-import SystemReportStore from "./systemReportStore";
 import UserStore from "./userStore";
 
 interface Store {
   commonStore: CommonStore;
   userStore: UserStore;
   modalStore: ModalStore;
-  systemReportStore: SystemReportStore;
 }
 
 export const store: Store = {
   commonStore: new CommonStore(),
   userStore: new UserStore(),
   modalStore: new ModalStore(),
-  systemReportStore: new SystemReportStore(),
 };
 
 export const StoreContext = createContext(store);
